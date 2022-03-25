3909a826bf10cbf48f829418c78fd11e9afab58f
Ferhatkara91 <ferh0322@stud.kea.dk>
2022-03-25 21:59:41 +0100

    updating code

--- delivery.py
+++ delivery.py
@@ -5,33 +5,43 @@ import dash_bootstrap_components as dbc
 import plotly.express as px
 import pandas as pd
 
-Excel_file = 'my_shop_data.xlsx'
-customers = pd.read_excel(Excel_file, "customers")
-order = pd.read_excel(Excel_file, "order")
-employee = pd.read_excel(Excel_file, "employee")
-products = pd.read_excel(Excel_file, "products")
+df1 = 'my_shop_data.xlsx'
+df2= pd.read_excel(df1, "customers")
+df3 = pd.read_excel(df1, "order")
+df4 = pd.read_excel(df1, "employee")
+df5 = pd.read_excel(df1, "products")
+
 
 def sales():
-    fig = px.bar(order, x='employee_id', y='quantity', title='Sales by employee')
+    fig = px.pie(df3, names='employee_id', values='quantity', title='Sales by Employee') 
+    fig.update_layout({
+        'plot_bgcolor':'rgb(175,153,153)',
+        'paper_bgcolor': 'rgb(175,153,153)',})
+    
     return fig
 
 def products():
-    fig = px.bar(order, x='product_id', y='quantity', title='Sales by products')
+    fig = px.pie(df3, names='product_id', values='quantity', title='Sales by Products')
+    fig.update_layout({
+        'plot_bgcolor':'rgb(175,153,153)',
+        'paper_bgcolor': 'rgb(175,153,153)',})
     return fig
 
 app = dash.Dash()
 
-app = dash.Dash(external_stylesheets = [ dbc.themes.FLATLY],)
+app = dash.Dash(external_stylesheets = [ dbc.themes.FLATLY],
+)
+
 
 body_app = dbc.Container([
     dbc.Row([
         dbc.Col(
             dcc.Graph(id = 'sales', figure = sales()),
-            style = {'height':'400px'},xs = 12, sm = 12, md = 6, lg = 6, xl = 6),
+            style = {'height':'750px', 'background-color':'rgb(175,153,153)' },xs = 12, sm = 12, md = 6, lg = 6, xl = 6),
          
         dbc.Col(
             dcc.Graph(id = 'products', figure = products()),
-            style = {'height':'400px'},xs = 12, sm = 12, md = 6, lg = 6, xl = 6), 
+            style = {'height':'750px', 'background-color':'rgb(175,153,153)'},xs = 12, sm = 12, md = 6, lg = 6, xl = 6), 
     ]),
 
 ],fluid = True) 
@@ -41,17 +51,19 @@ topbar = dbc.Navbar(
         dbc.Row(
             [
                 dbc.Col(
-                    dbc.NavbarBrand("Graphs", style = {'color':'black', 'fontSize':'25px','fontFamily':'Times New Roman'}
+                    dbc.NavbarBrand("Pie-charts of excel spreadsheet", style = {'color':'#460101', 'fontSize':'100px','fontFamily':'Times New Roman'}
                     ),
                 )
             ],
-            align="center",
+            align="right",
             className="g-10",
         ),
     ]
 )
 
-app.layout = html.Div(id = 'parent', children = [topbar, body_app])
+app.layout = html.Div(id = 'parent', children = [topbar, body_app]
+
+)
 
 
 if __name__ == "__main__":
